2722f43253e147ba1b301b44470bf11d2bd7d438
lashaiakobadze <70689627+lashaiakobadze@users.noreply.github.com>
2021-02-25 02:27:18 +0400

    Add async function

--- javsacript/javascript.js
+++ javsacript/javascript.js
@@ -8,46 +8,79 @@ const day = String(dateObj.getDate()).padStart(2, '0');
 const year = dateObj.getFullYear();
 const output = month  + '\n'+ day  + '.' + year;
 
-function getData (url) {
-    return fetch(url)
-    .then(response => response.json())
-    .then(data => { return data })
-}
+// fetch helper function
+async function getData(url) {
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
 
-const time = document.querySelectorAll('.time');
-function loadWorldStats() {
-    getData(`https://disease.sh/v3/covid-19/all`).then(data => {
-        document.querySelectorAll('.cases')[0].innerHTML = data.cases.toLocaleString();
-        document.querySelectorAll('.cases')[1].innerHTML = data.recovered.toLocaleString();
-        document.querySelectorAll('.cases')[2].innerHTML = data.critical.toLocaleString();
-        document.querySelectorAll('.cases')[3].innerHTML = data.deaths.toLocaleString();
+    if(!response.ok) throw new Error(`${data.message} (${response.status})`);
 
-        time.forEach(t => t.innerHTML = output);
-    });
+    return data;
+  } catch (err) {
+    throw err;
+  };
 };
 
-function loadWorldCountry() {
-    getData(`https://disease.sh/v3/covid-19/countries`).then(data => {
-        let text = '';
+let countriesData = '';
+const worldUrl = 'https://disease.sh/v3/covid-19/all';
+const countryUrl = "https://disease.sh/v3/covid-19/countries";
 
-        for(let x = 0; x < data.length; x++) {
-            text += `
-            <div class="country">
-                <img class="country_flag" src="${data[x].countryInfo.flag}" alt="cointry-flag">
-                <div class="country_name">${data[x].country}</div>
-                <div class="country_new">+${data[x].todayCases}</div>
-                <div class="country_new-recovered">+${data[x].todayRecovered}</div>
-                <i class="fas fa-thumbtack country_favorite"></i>
-            </div>
-            `;
-            document.querySelector('.countries-container').innerHTML = text;
-        };
-    });
-}
+// world information
+const time = document.querySelectorAll('.time');
+const cases = document.querySelectorAll('.cases');
+time.forEach(t => t.innerHTML = output);
+
+async function loadWorldStats() {
+    const worldData = await getData(worldUrl);
+    cases[0].innerHTML = worldData.cases.toLocaleString();
+    cases[1].innerHTML = worldData.recovered.toLocaleString();
+    cases[2].innerHTML = worldData.critical.toLocaleString();
+    cases[3].innerHTML = worldData.deaths.toLocaleString();
+};
 
 loadWorldStats();
+
+// country information
+async function loadWorldCountry() {
+  countriesData = await getData(countryUrl);
+
+  let text = '';
+  let html = '';
+
+  countriesData.forEach( data => {
+    text += `
+    <div class="country">
+        <img class="country_flag" src="${data.countryInfo.flag}" alt="${data.country}-flag">
+        <div class="country_name" data-id="${data.updated}">${data.country}</div>
+        <div class="country_new">+${data.todayCases}</div>
+        <div class="country_new-recovered">+${data.todayRecovered}</div>
+        <i class="fas fa-thumbtack country_favorite"></i>
+    </div>
+    `;
+    document.querySelector('.countries-container').innerHTML = text;
+
+    html += `
+    <div class="country-information_detals">
+        <img class="country-information_detals_flag" src="${data.countryInfo.flag}" alt="${data.country}-flag">
+        <div class="country-information_detals_name">${data.country}</div>
+        <div class="country-information_detals_confrimed">${data.cases}</div>
+        <div class="country-information_detals_recovered">${data.recovered}</div>
+        <div class="country-information_detals_critical">${data.critical}</div>
+        <div class="country-information_detals_death">${data.deaths}</div>
+    </div>
+    `;
+    document.querySelector('.country-information-container').innerHTML = html;
+  });
+
+  console.log(countriesData);
+}
+
 loadWorldCountry();
 
+console.log(countriesData);
+
+// search function
 function myFunction() {
   let input, filter, tr, td, i, txtValue;
   input = document.querySelector(".country_search");
@@ -65,5 +98,93 @@ function myFunction() {
       }
     }
   }
+};
+
+
+/*
+// Render countr data from country data array
+
+function country(flag, countryName, todayCases, todayRecovered, cases, recovered, critical, deaths) {
+  this.flag = flag;
+  this.countryName = countryName;
+  this.todayCases = todayCases;
+  this.todayRecovered = todayRecovered;
+  this.cases = cases;
+  this.recovered = recovered;
+  this.critical = critical;
+  this.deaths = deaths;
 }
 
+async function inputData() {
+  getData(`https://disease.sh/v3/covid-19/countries`).then(data => {
+      for(let i = 0; i < data.length; i++) {
+        const newCountry = new country(data[i].countryInfo.flag, data[i].country, data[i].todayCases, data[i].todayRecovered, data[i].cases, data[i].recovered, data[i].critical, data[i].deaths)
+        countriesData.push(newCountry);
+        // countriesData.push([data[i].countryInfo.flag, data[i].country, data[i].todayCases, data[i].todayRecovered, data[i].cases, data[i].recovered, data[i].critical, data[i].deaths]);
+      };
+  });
+
+
+  // let text = '';
+
+  // for(let i = 0; i < countriesData.length; i++) {
+  //     text += `
+  //     <div class="country">
+  //         <img class="country_flag" src="${countriesData[i][0]}" alt="cointry-flag">
+  //         <div class="country_name">${countriesData[i][1]}</div>
+  //         <div class="country_new">+${countriesData[i][2]}</div>
+  //         <div class="country_new-recovered">+${countriesData[i][3]}</div>
+  //         <i class="fas fa-thumbtack country_favorite"></i>
+  //     </div>
+  //     `;
+  //   };
+  //   document.querySelector('.countries-container').innerHTML = text;
+};
+
+inputData();
+
+function renderCountry(country) {
+  let html = '';
+      text += `
+      <div class="country">
+          <img class="country_flag" src="${country.flag}" alt="cointry-flag">
+          <div class="country_name">${country.countryName}</div>
+          <div class="country_new">+${country.todayCases}</div>
+          <div class="country_new-recovered">+${country.todayRecovered}</div>
+          <i class="fas fa-thumbtack country_favorite"></i>
+      </div>
+      `;
+  document.querySelector('.countries-container').insertAdjacentHTML('beforeend', html);
+}
+
+async function vaxtang() {
+  await countriesData.map(data => renderCountry(data));
+  if(countriesData.length <= 0) console.log('jer ar Sevsebula');
+};
+
+vaxtang();
+
+console.log(countriesData);
+
+// // country data
+// function loadWorldCountry() {
+    // let text = '';
+
+    // for(let x = 0; x < countriesData.length; x++) {
+    //     text += `
+    //     <div class="country">
+    //         <img class="country_flag" src="${countriesData[x].flag}" alt="cointry-flag">
+    //         <div class="country_name">${countriesData[x].country}</div>
+    //         <div class="country_new">+${countriesData[x].todayCases}</div>
+    //         <div class="country_new-recovered">+${countriesData[x].todayRecovered}</div>
+    //         <i class="fas fa-thumbtack country_favorite"></i>
+    //     </div>
+    //     `;
+    //     document.querySelector('.countries-container').innerHTML = text;
+    // };
+
+//     console.log('darenderda');
+// }
+
+// setTimeout(loadWorldCountry, 100000);
+*/
